7bcc6436b6b11730ccbbe001f47d10999513efaa
bmstine <76221866+bmstine@users.noreply.github.com>
2023-11-28 19:50:07 -0600

    final update

--- final418/script.js
+++ final418/script.js
@@ -4,28 +4,33 @@ $(document).ready(function() {
     // Initialize tabs
     $("#tabs").tabs();
 
-    // Function to fetch weather data
     function fetchWeather(latitude, longitude) {
-        var apiKey = '5b690168a187ec4f3b5291c572967625'; // Replace with your API key
-        var url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-        $.ajax({
-            url: url,
-            type: 'GET',
-            dataType: 'json',
-            success: function(weatherData) {
-                var currentTemp = weatherData.main.temp;
-                var tempHigh = weatherData.main.temp_max;
-                var tempLow = weatherData.main.temp_min;
-
-                $('#current-temp').text(`Current Temp: ${currentTemp} °C`);
-                $('#high-low').text(`High: ${tempHigh} °C / Low: ${tempLow} °C`);
-            },
-            error: function(error) {
-                console.log('Error fetching and parsing data', error);
-            }
+        var apiKey = '5b690168a187ec4f3b5291c572967625'; // Replace with your actual OpenWeatherMap API key
+        var url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`; // Using imperial units for Fahrenheit
+    
+        // Immediately display a loading message
+        $('#weather-info').text('Weather for your area is loading...');
+    
+        fetch(url)
+        .then(response => response.json())
+        .then(weatherData => {
+            var currentTemp = weatherData.main.temp;
+            var tempHigh = weatherData.main.temp_max;
+            var tempLow = weatherData.main.temp_min;
+    
+            // Replace the loading message with actual data
+            $('#weather-info').html(
+                `<div id="current-temp">Current Temp: ${currentTemp} °F</div>` +
+                `<div id="high-low">High: ${tempHigh} °F / Low: ${tempLow} °F</div>`
+            );
+        })
+        .catch(error => {
+            // Replace the loading message with an error message
+            $('#weather-info').text('Error loading weather data.');
+            console.log('Error fetching and parsing data', error);
         });
     }
+    
 
     // Function to get user's location
     function getLocation() {
